c0f769874ead59ac70d888db5b9e257424f817ba
Liamsi007 <32396182+Liamsi007@users.noreply.github.com>
2019-06-25 17:52:43 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function myfunction(){
+document.getElementById("name").value="";
+document.getElementById("email").value="";
+document.getElementById("subject").value="";
+document.getElementById("message").value="";
+alert("Your email was sent successfully, Thx !");
+}
